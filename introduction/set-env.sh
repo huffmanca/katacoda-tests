47e4b0474fd0fe8d884bbc2e4c917364bd326ca7
Christian Huffman <chuffman@redhat.com>
2018-04-19 13:36:45 -0400

    Updated start script.

--- introduction/set-env.sh
+++ introduction/set-env.sh
@@ -1,7 +1,8 @@
 export JAVA_HOME=$(jrunscript -e 'java.lang.System.out.println(java.lang.System.getProperty("java.home"));')
-cd ${HOME}/projects/monolith
-git pull --quiet
 clear; echo; echo -e "\033[0;32mYour ip-address for this cluster is $(hostname -i), please record this so that the instructor can reboot your machine if needed.\033[0m";echo
-echo "Attempting to pull docker image."
+echo -e "Attempting to pull docker image."
 docker pull windup3/windup-cli-openshift
-echo "Pull completed."
+docker pull httpd
+
+docker run -it -v /home/scrapbook/tutorial/src_example.zip:/opt/jboss/application:rw windup3/windup-cli-openshift windup/bin/windup --input /opt/jboss/application --target eap7
+
