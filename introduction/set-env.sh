f07d2a4bb1bbd6d97f1535bcc70931382435c88e
Christian Huffman <chuffman@redhat.com>
2018-04-19 14:05:23 -0400

    Testing.

--- introduction/set-env.sh
+++ introduction/set-env.sh
@@ -1,8 +1,8 @@
 export JAVA_HOME=$(jrunscript -e 'java.lang.System.out.println(java.lang.System.getProperty("java.home"));')
 clear; echo; echo -e "\033[0;32mYour ip-address for this cluster is $(hostname -i), please record this so that the instructor can reboot your machine if needed.\033[0m";echo
 echo -e "Attempting to pull docker image."
-docker pull windup3/windup-cli-openshift
-docker pull httpd
+# docker pull windup3/windup-cli-openshift
+# docker pull httpd
 
-docker run -it -v /home/scrapbook/tutorial/src_example.zip:/opt/jboss/application:rw windup3/windup-cli-openshift windup/bin/windup --input /opt/jboss/application --target eap7
+# docker run -it -v /home/scrapbook/tutorial/src_example.zip:/opt/jboss/application:rw windup3/windup-cli-openshift windup/bin/windup --input /opt/jboss/application --target eap7
 
